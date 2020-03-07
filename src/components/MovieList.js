53a46272355885c77a77e148f466c9e965cba894
DJCoraxe <djcoraxe@gmail.com>
2020-03-06 19:02:10 -0700

    messed up this CSS super bad

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import TrackWatched from './TrackWatched'
+import './movieList.module.css'
 
-const MovieList = ({ onClick, item: { title, watched} }) => {
+const MovieList = ({ onClick, item: { title, watched} }) => 
+{
   return(
       <li>{title} <TrackWatched onClick={onClick} className={(watched) ? "watched" : "unwatched"} id={title}/></li>
   )
--- src/components/TrackWatched.js
+++ src/components/TrackWatched.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+
 const TrackWatched = ( {id, onClick, className} ) => {
   return(
     <span id={id} onClick={onClick} className={(className)}>{className.toString()}</span>
